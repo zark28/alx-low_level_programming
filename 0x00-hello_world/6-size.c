da7f281519b36390f0d66fe8849e6a8959feeeea
zark28 <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>
2022-04-07 17:27:42 +0000

    My first c programm work10

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,15 @@
 */
 int main(void)
 {
-printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
-printf("Size an int: %lu byte(s)\n", (unsigned long)sizeof(int));
-printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
-printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(long long int));
-printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
+char a;
+int b;
+long int c;
+long long int d;
+float f;
+printf("Size of a char: %lu byte(s)\n", (unsigned)sizeof(a));
+printf("Size of an int: %lu byte(s)\n", (unsigned)sizeof(b));
+printf("Size of a long int: %lu byte(s)\n", (unsigned)sizeof(c));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned)sizeof(d));
+printf("Size of a float: %lu byte(s)\n", (unsigned)sizeof(f));
 return (0);
 }
