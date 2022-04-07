9bc2d39d17cc289072d3add1b0aee92ebf5b0508
zark28 <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>
2022-04-07 15:50:44 +0000

    My first c programm work6

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-*main -prints exactly "and the piece of art is useful" - Dora Korpar, 2015-10-19"
+*main -prints exactly "and the piece of art is useful" 
+*- Dora Korpar, 2015-10-19"
 *followed by a new line, to the standars error
-*Retun: 0 for sucess
+*Return: 0 for sucess
 */
 int main(void)
 {
