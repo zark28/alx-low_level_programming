639e2170f687f3586c49cf5890db23fface211fc
zark28 <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>
2022-04-14 23:08:01 +0000

    My sixth c programing work3

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,8 +1,8 @@
 #include "main.h"
-
 /**
  * print_line - draws a straight line in the terminal
- * @n: number of times the character _ should be printed
+ * @n: an input integer 
+  *Return:Always 0
  */
 void print_line(int n)
 {
@@ -12,7 +12,6 @@ _putchar('\n');
 } else
 {
 int i;
-
 for (i = 1; i <= n; i++)
 {
 _putchar('_');
